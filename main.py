0e6f578bfb3848a3fcbb3741cc3a0f9e744cb0fd
davidprevite-cloud <davidprevite@gmail.com>
2026-06-27 14:17:08 +0200

    Inicializar proyecto_1 con estructura base

---
+++ main.py
@@ -0,0 +1,8 @@
+def saludar(nombre):
+                    
+    return f"Hola, {nombre}! Bienvenido a ML."
+
+if __name__ == '__main__':
+    mensaje = saludar("Estudiante")                                                                                                     
+    print(mensaje)
+
